85011f6225821489a6a0d77c280b241f26880206
IsraelRufus-RJ <israelrufus2000@gmail.com>
2026-05-31 22:57:42 +0530

    Initial clean commit

---
+++ src/main/java/com/ramjo/spring_security_demo/Spring_Security_Demo/SpringSecurityDemoApplication.java
@@ -0,0 +1,14 @@
+package com.ramjo.spring_security_demo.Spring_Security_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDemoApplication.class, args);
+		System.out.println("Spring Started ...>");
+	}
+
+}
---
+++ src/test/java/com/ramjo/spring_security_demo/Spring_Security_Demo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjo.spring_security_demo.Spring_Security_Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
